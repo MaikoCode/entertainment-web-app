5b8260fa0fd26dddc9ad89f3d68d1573a223d9de
Maikoke <mamadou.maikoke@webpick.net>
2024-01-22 11:40:03 +0100

    Update layout and NavBar components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* <body className={inter.className}>{children}</body> */}
-      <body className={`${outfit.variable} bg-dark-blue text-white`}>
+      <body className={`${outfit.variable} bg-deep-dark text-white`}>
         {children}
       </body>
     </html>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,22 @@
 import  Link  from "next/link";
 import  Image  from "next/image";
-import MovieIcon from "../assets/icon-nav-movies.svg"
+import movieIcon from "../assets/icon-nav-movies.svg"
+import saveIcon from "../assets/icon-nav-bookmark.svg"
+import avatarIcon from "../assets/image-avatar.png"
+import iconNav from "../assets/icon-nav-home.svg"
+import seriesIcon from "../assets/icon-nav-tv-series.svg"
+import logoIcon from "../assets/logo.svg"
+
+
 
 export default function NavBar() {
 
-    return <nav className="flex justify-between border rounded-lg">
+    return <nav className="bg-dark-blue flex justify-between items-center p-5">
         <ul>
             <li>
                 <Link href="/">
                    <Image 
-                   src={MovieIcon}
+                   src={logoIcon}
                    alt="icon"
                    />
                 </Link>
@@ -17,12 +24,53 @@ export default function NavBar() {
         </ul>
 
 
-        <ul>
+        <ul className="flex space-x-2">
+        <li>
+                <Link href="/">
+                   <Image 
+                   src={iconNav}
+                   alt="icon"
+                   />
+                </Link>
+            </li>
+        <li>
+                <Link href="/">
+                   <Image 
+                   src={movieIcon}
+                   alt="icon"
+                   />
+                </Link>
+            </li>
+
+            <li>
+                <Link href="/">
+                   <Image 
+                   src={seriesIcon}
+                   alt="icon"
+                   />
+                </Link>
+            </li>
+
+            <li>
+                <Link href="/">
+                   <Image 
+                   src={saveIcon}
+                   alt="icon"
+                   />
+                </Link>
+            </li>
 
         </ul>
 
 
-        <span></span>
+        <span>
+
+                <Image 
+                src={avatarIcon}
+                alt="icon"
+                className="h-8 w-8 rounded-full"
+                />
+        </span>
         
     </nav>
 
